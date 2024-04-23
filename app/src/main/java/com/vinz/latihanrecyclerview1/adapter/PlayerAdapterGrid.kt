6692a98d611fad3b17e1dbd315dcdd74c1b5fbe1
Kevin M <kevinmalikf@gmail.com>
2024-04-23 19:04:13 +0700

    Initial commit

---
+++ app/src/main/java/com/vinz/latihanrecyclerview1/adapter/PlayerAdapterGrid.kt
@@ -0,0 +1,53 @@
+package com.vinz.latihanrecyclerview1.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.textview.MaterialTextView
+import com.vinz.latihanrecyclerview1.R
+import com.vinz.latihanrecyclerview1.data.PlayerData
+
+class PlayerAdapterGrid(private val playerList: List<PlayerData>) : RecyclerView.Adapter<PlayerAdapterGrid.PlayerViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: PlayerData)
+    }
+
+    class PlayerViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val playerName: MaterialTextView = itemView.findViewById(R.id.player_name)
+        val playerDescription: MaterialTextView = itemView.findViewById(R.id.player_description)
+        val playerImage: ShapeableImageView = itemView.findViewById(R.id.player_image)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PlayerViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_player_grid, parent, false)
+        return PlayerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlayerViewHolder, position: Int) {
+        val data = playerList[position]
+
+        holder.playerName.text = data.name
+        holder.playerDescription.text = data.description.shorten(85)
+        holder.playerImage.setImageResource(data.image)
+
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(playerList[holder.adapterPosition]) }
+    }
+
+    override fun getItemCount(): Int = playerList.size
+
+    private fun String.shorten(maxLength: Int): String {
+        return if (this.length <= maxLength) this else "${this.substring(0, maxLength)}..."
+    }
+}
---
+++ app/src/main/java/com/vinz/latihanrecyclerview1/data/PlayerData.kt
@@ -0,0 +1,7 @@
+package com.vinz.latihanrecyclerview1.data
+
+data class PlayerData (
+    val name: String,
+    val description: String,
+    val image: Int
+)
